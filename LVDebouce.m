271c7a89001a6f354c9675e733744db24cfd5a4c
Kelly Sutton <michael.k.sutton@gmail.com>
2013-07-25 12:24:16 -0400

    Done.

---
+++ LVDebouce.m
@@ -0,0 +1,38 @@
+//
+//  LVDebounce.m
+//  LayerVault
+//
+//  Created by Kelly Sutton on 7/23/13.
+//  Copyright (c) 2013 LayerVault. Licensed under the MIT License.
+//
+
+#import "LVDebounce.h"
+
+static NSMutableDictionary *timers = nil;
+
+@implementation LVDebounce
+
++ (void)initialize {
+    if (self == [LVDebounce class]) {
+        timers = [NSMutableDictionary dictionary];
+    }
+}
+
++ (void)fireAfter:(NSTimeInterval)seconds target:(id)target selector:(SEL)aSelector userInfo:(id)userInfo {
+    @synchronized(self) {
+        NSArray *eventKey = @[target, NSStringFromSelector(aSelector)];
+        if ([timers objectForKey:eventKey]) {
+            NSTimer *timer = [timers objectForKey:eventKey];
+
+            if ([timer respondsToSelector:@selector(invalidate)])
+                [timer invalidate];
+
+            [timers removeObjectForKey:eventKey];
+        }
+
+        [timers setObject:[NSTimer scheduledTimerWithTimeInterval:seconds target:target selector:aSelector userInfo:userInfo repeats:NO]
+                   forKey:eventKey];
+    }
+}
+
+@end
---
+++ LVDebounce.h
@@ -0,0 +1,15 @@
+//
+//  LVDebounce.h
+//  LayerVault
+//
+//  Created by Kelly Sutton on 7/23/13.
+//  Copyright (c) 2013 LayerVault. Licensed under the MIT License.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LVDebounce : NSObject
+
++ (void)fireAfter:(NSTimeInterval)seconds target:(id)target selector:(SEL)aSelector userInfo:(id)userInfo;
+
+@end
